3fd8c43721d62bc081276bc39d57985c95b08f07
leeminju <leemj4090@naver.com>
2023-11-10 15:55:22 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 2.96 ms, Memory: 78.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
@@ -0,0 +1,22 @@
+class Solution {
+    public String solution(String s) {
+        String answer = "";
+        int j=0;
+        for(int i=0;i<s.length();i++,j++){    
+            char c = s.charAt(i);
+            
+            if(c == ' '){
+                j = -1;
+                answer += " ";
+                continue;
+            }
+        
+            if(j % 2 == 0){
+                answer += Character.toString(c).toUpperCase();
+            }else{
+                answer += Character.toString(c).toLowerCase();
+            }
+        } 
+        return answer;
+    }
+}
