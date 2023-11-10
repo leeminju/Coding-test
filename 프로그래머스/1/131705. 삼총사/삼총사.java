748289ad157baa92bba5bb3168cf45142cc7abfc
leeminju <leemj4090@naver.com>
2023-11-10 18:08:20 +0900

    [level 1] Title: 삼총사, Time: 0.03 ms, Memory: 77.2 MB -BaekjoonHub

--- 프로그래머스/1/131705. 삼총사/삼총사.java
+++ 프로그래머스/1/131705. 삼총사/삼총사.java
@@ -3,8 +3,17 @@ class Solution {
     int answer = 0;
      public int solution(int[] number) {
 
-        visited = new boolean[number.length];
-        dfs(number, 0, 0, 0);
+        for(int i=0; i<number.length-2; i++){
+            for(int j=i+1; j<number.length-1; j++){
+                for(int k=j+1; k<number.length; k++){
+                    if(number[i]+number[j]+number[k]==0) answer++;
+                }
+            }
+        }
+
+         
+     //   visited = new boolean[number.length];
+    //    dfs(number, 0, 0, 0);
         return answer;
     }
 
