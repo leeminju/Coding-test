da41b2195b1416d87d9e35e6e2c28d1dccbf489c
leeminju <leemj4090@naver.com>
2023-11-13 16:54:06 +0900

    체육복 upload

---
+++ 이것이 코딩테스트다/그리디/숫자카드게임.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int N = sc.nextInt();
+    int M = sc.nextInt();
+
+    int max = 0;
+    for(int i=0;i<N;i++){
+      int min = 10001;
+      for(int j=0;j<M;j++){
+        int num = sc.nextInt();
+        min = Math.min(min,num);
+      }
+      max = Math.max(max,min);
+    }
+
+    System.out.println(max);
+  
+  }
+}
