0ebfae7a9b560f3c1bae05f62ecd9bc3d42ee5d9
leeminju <leemj4090@naver.com>
2023-11-14 10:22:00 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 9.16 ms, Memory: 76.1 MB -BaekjoonHub

--- 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.java
+++ 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.java
@@ -1,9 +1,31 @@
+import java.util.*;
+
 class Solution {
     public String solution(String[] seoul) {
+        
         for(int i=0;i<seoul.length;i++){
             if(seoul[i].equals("Kim"))
                 return "김서방은 "+i+"에 있다";
         }
+        /*
+        int start = 0;
+        int last = seoul.length;
+        int mid = seoul.length/2;
+       
+         
+        while(start<last){
+            mid = (start+last)/2;
+            if(seoul[mid].equals("Kim")){
+                return "김서방은 "+mid+"에 있다";
+            }
+            
+            String[] front=Arrays.copyOfRange(seoul,start, mid);
+            if(Arrays.asList(front).contains("Kim")){
+                last = mid;
+            }else{
+                start = mid;
+            }
+        }*/
         return "";
     }
 }
