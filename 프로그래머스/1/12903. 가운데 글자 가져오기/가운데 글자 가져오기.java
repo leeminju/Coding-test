1c05a1a43152dcee377977b56f1024e66b0c9731
leeminju <leemj4090@naver.com>
2023-11-16 10:03:54 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 10.89 ms, Memory: 73.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String solution(String s) {
+        String answer = "";
+        int mid = s.length()/2;
+        if(s.length()%2 ==0 ){
+            //짝수면
+            answer += s.charAt(mid-1);
+            answer += s.charAt(mid);
+        }else{
+            answer += s.charAt(mid);
+        }
+        
+        
+        return answer;
+    }
+}
