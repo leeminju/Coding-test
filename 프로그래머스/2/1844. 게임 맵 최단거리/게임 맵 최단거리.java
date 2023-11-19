d4ce8e541cb36ad1af70854ee9861325f138edf6
leeminju <leemj4090@naver.com>
2023-11-20 01:00:58 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 5.77 ms, Memory: 54.6 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,52 +1,45 @@
 import java.util.*;
 class Solution {
     public int solution(int[][] maps) {
-        int answer = 0;
         int n = maps.length;
         int m = maps[0].length;
-        Queue<Integer[]> queue = new LinkedList<>();
-        int[] dx = {0,0,-1,1};
-        int[] dy = {-1,1,0,0};
-        boolean[][] visited = new boolean[n][m];
-        visited[0][0] = true;
+        int[] dx = {0, 0, -1, 1};
+        int[] dy = {-1, 1, 0, 0};
 
-        //0은 벽이 있는 자리, 1은 벽이 없는 자리를 나타냅니다.
-        //처음에 캐릭터는 게임 맵의 좌측 상단인 (1, 1)->0,0 위치에 있으며
-        //상대방 진영은 게임 맵의 우측 하단인 (n, m)->n-1,m-1 위치에 있습니다.
-        queue.add(new Integer[]{0,0,1});
-        
-        while(true){
-            Integer[] node=queue.poll();
-            int x=node[0];
-            int y=node[1];
-            int cnt = node[2];
+        Queue<Integer[]> queue = new LinkedList<>();
+        queue.offer(new Integer[]{0, 0});
+
+        while (!queue.isEmpty()) {
+            Integer[] node = queue.poll();
+            int x = node[0];
+            int y = node[1];
             
             if(x == n-1 && y== m-1){
-                answer = cnt;
-                break;
+                return maps[n - 1][m - 1];
             }
-            
-            for(int i=0;i<4;i++){
-                int nx = x + dx[i];
-                int ny = y + dy[i];
-                
-                if(nx < 0 || nx > n-1 || ny<0 || ny> m-1){
+
+            for (int k = 0; k < 4; k++) {
+                int nx = x + dx[k];
+                int ny = y + dy[k];
+
+                if (nx < 0 || nx >= n || ny < 0 || ny >= m) {
                     continue;
                 }
-                
-                if(maps[nx][ny] == 1 && !visited[nx][ny]){
-                    visited[nx][ny] = true;
-                    queue.offer(new Integer[]{nx,ny,cnt+1});
+
+                if (maps[nx][ny] == 0) {
+                    continue;
+                }
+
+                if (maps[nx][ny] == 1) {
+                    maps[nx][ny] = maps[x][y] + 1;//방문처리
+                    queue.offer(new Integer[]{nx, ny});
                 }
             }
-            
-            if(queue.isEmpty())
-                return -1;
-            
         }
+
         
         
         
-        return answer;
+        return -1;
     }
 }
