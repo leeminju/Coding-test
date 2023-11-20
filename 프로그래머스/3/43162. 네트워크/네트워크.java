703b6dca622d211c6d47380d291d300a663f205e
leeminju <leemj4090@naver.com>
2023-11-20 09:33:58 +0900

    [level 3] Title: 네트워크, Time: 0.09 ms, Memory: 79 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -2,25 +2,11 @@ import java.util.*;
 class Solution {
     public int solution(int n, int[][] computers) {
         int answer = 0;
-        ArrayList<ArrayList<Integer>> graph = new ArrayList<ArrayList<Integer>>();
-        boolean[] visited = new boolean[n];
-        
-           // 그래프 초기화
-        for (int i = 0; i < n; i++) {
-            graph.add(new ArrayList<Integer>());
-        }
+        boolean[] visited= new boolean[n];
         
         for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                if(i!= j && computers[i][j] == 1){
-                    graph.get(i).add(j);
-                }
-            }
-        }
-        
-        for(int i=0;i<n;i++){
-            if(visited[i] == false){
-                dfs(graph,i,visited);
+            if(!visited[i]){
+                dfs(computers,i,visited);
                 answer++;
             }
         }
@@ -28,12 +14,12 @@ class Solution {
         return answer;
     }
     
-     private static void dfs(ArrayList<ArrayList<Integer>> graph, int v, boolean[] visited) {
+     private static void dfs(int[][] array, int v, boolean[] visited) {
         visited[v] = true;
 
-        for (Integer node : graph.get(v)) {
-            if (!visited[node]) {
-                dfs(graph, node, visited);
+        for (int i=0;i<array[v].length ; i++) {
+            if (array[v][i] == 1 &&   !visited[i]) {
+                dfs(array, i, visited);
             }
         }
 
