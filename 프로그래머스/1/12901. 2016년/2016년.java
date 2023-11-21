3cdda6eac4fa412a6cdafcecde8a1659612412cf
leeminju <leemj4090@naver.com>
2023-11-21 09:17:48 +0900

    [level 1] Title: 2016년, Time: 0.02 ms, Memory: 75.9 MB -BaekjoonHub

--- 프로그래머스/1/12901. 2016년/2016년.java
+++ 프로그래머스/1/12901. 2016년/2016년.java
@@ -3,13 +3,15 @@ class Solution {
         //2월 29일 존재함!
         int sum = 0;
         
-        String[] dayOfWeek = {"FRI","SAT","SUN","MON","TUE","WED","THU"};
+        String[] dayOfWeek = {"THU","FRI","SAT","SUN","MON","TUE","WED"};
+        //1월 7일 목요일 7%7 = 0 0번째 인덱스
+        //1월 1일 금요일 7%1 = 1 1번째 인덱스
         int[] day = {0,31,29,31,30,31,30,31,31,30,31,30,31};
         
         for(int i=0;i< a;i++){
             sum += day[i];
         }
-        int q = (sum+b-1)%7;
+        int q = (sum+b)%7;
         
         return dayOfWeek[q];
         
