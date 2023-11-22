f8472a4e9e6e2e7d051d9cd3b2a4d11cd4f821d4
leeminju <leemj4090@naver.com>
2023-11-22 09:07:28 +0900

    [level 2] Title: 가장 큰 수, Time: 1.51 ms, Memory: 77.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -0,0 +1,35 @@
+import java.util.*;
+class Solution {
+    public String solution(int[] numbers) {
+        String answer = "";
+        String[] strs = new String[numbers.length];
+        
+        for(int i=0;i<strs.length;i++){
+            strs[i] = Integer.toString(numbers[i]);
+        }
+        
+        Arrays.sort(strs, new Comparator<String>(){            
+            @Override
+            public int compare(String o1, String o2){
+                if(o1.length() == o2.length()){
+                    return o2.compareTo(o1);
+                }else{
+                    return (o2+o1).compareTo(o1+o2);
+                }
+            }
+        });
+        
+    
+        
+        for(String str : strs){
+            answer += str;
+        }
+        
+        if(answer.charAt(0)=='0'){
+            return "0";
+        } 
+        
+        
+        return answer;
+    }
+}
