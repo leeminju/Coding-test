42975192a5433bd0dc46dca1ac51032b90cecd30
leeminju <leemj4090@naver.com>
2023-11-24 09:05:39 +0900

    [level 1] Title: 소수 만들기, Time: 0.03 ms, Memory: 69.8 MB -BaekjoonHub

--- 프로그래머스/1/12977. 소수 만들기/소수 만들기.java
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.java
@@ -19,7 +19,7 @@ class Solution {
     }
     
     private boolean isPrimeNumber(int n){
-        for(int i=2;i<n;i++){
+        for(int i=2;i<=Math.sqrt(n);i++){
             if(n%i ==0){
                 return false;
             }
