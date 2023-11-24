98228ea99a519cbbabbe9515e77dac0e6b43eeb2
leeminju <leemj4090@naver.com>
2023-11-24 09:04:56 +0900

    [level 1] Title: 소수 만들기, Time: 0.03 ms, Memory: 70.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int solution(int[] nums) {
+        int answer = 0;
+        int len =nums.length;
+        //소수 :  1과 자기 자신만을 약수로 가지는 수
+        
+        for(int i=0;i<len-2;i++){
+            for(int j=i+1;j<len-1;j++){
+                for(int k=j+1;k<len;k++){
+                    int sum =nums[i]+nums[j]+nums[k];
+                    if(isPrimeNumber(sum)){
+                      answer++; 
+                    }
+                }
+            }
+        }
+
+        return answer;
+    }
+    
+    private boolean isPrimeNumber(int n){
+        for(int i=2;i<n;i++){
+            if(n%i ==0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
