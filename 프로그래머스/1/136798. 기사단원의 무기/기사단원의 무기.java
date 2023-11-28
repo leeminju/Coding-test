967e89f642d283b1efd684dfbd96d9d0daeafd09
leeminju <leemj4090@naver.com>
2023-11-28 09:00:03 +0900

    [level 1] Title: 기사단원의 무기, Time: 0.15 ms, Memory: 77 MB -BaekjoonHub

--- 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.java
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.java
@@ -2,24 +2,24 @@ import java.util.*;
 
 class Solution {
     public int solution(int number, int limit, int power) {
-        int answer = 0;
+        int answer = 1;// 1은 약수의 개수가 1
+        
         int[] array = new int[number+1];//0부터 number까지
-        Arrays.fill(array,1);// 모두 1로 통일 (모두 1을 약수를 가지고 있기 때문에)        
-    
+        Arrays.fill(array,1);// 모두 1로 통일 (모두 1을 약수를 가지고 있기 때문에)                
+        
+        //에라토스테네스의 체와 비슷한 방식
         for(int i=2;i<=number;i++){
             for(int j=i;j<=number;j+=i){            
                 array[j]++;
             }
-        }
-        
-    
-        for(int n=1;n<=number;n++){ 
-            if(array[n] > limit){
+            
+            if(array[i] > limit){
                 answer += power;
             }else{
-                answer += array[n]; 
+                answer += array[i]; 
             }
         }
+        
         
         return answer;
     }
