b02588eb2dd9d76a2bd1f10c0513bf20500d53a4
leeminju <leemj4090@naver.com>
2023-11-30 10:09:42 +0900

    [level 1] Title: 체육복, Time: 0.02 ms, Memory: 77.2 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.java
+++ 프로그래머스/1/42862. 체육복/체육복.java
@@ -5,6 +5,8 @@ class Solution {
         int answer = n;
         int[] count = new int[n];
         
+        //문제에서 lost나 reserve가 정렬되어 있다는 언급이 없기 때문에 예제만 보고 정렬되어 있다고 가정하면 안 됩니다.
+        Arrays.fill(count,1);
         
         for(int num:reserve){
             count[num-1]++;
@@ -15,12 +17,12 @@ class Solution {
         }
       
         for(int i=0;i<n;i++){
-            if(count[i]== -1){
+            if(count[i]== 0){
                 //체육복 없다.
-                if(i>0 && count[i-1] == 1){
+                if(i>0 && count[i-1] == 2){
                     count[i-1]--;
                     count[i]++;
-                }else if(i<n-1 && count[i+1]==1){
+                }else if(i<n-1 && count[i+1]==2){
                     count[i+1]--;
                     count[i]++;
                 }else{
@@ -28,8 +30,6 @@ class Solution {
                 }
             }   
         }
-        
-        
         
         return answer;
     }
