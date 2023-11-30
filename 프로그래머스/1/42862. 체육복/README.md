816dca42c58fc8c78309c5afee235115fc4bdfa0
leeminju <leemj4090@naver.com>
2023-11-30 10:01:49 +0900

    [level 1] Title: 체육복, Time: 0.03 ms, Memory: 75.7 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/README.md
+++ 프로그래머스/1/42862. 체육복/README.md
@@ -1,10 +1,10 @@
 # [level 1] 체육복 - 42862 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42862?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42862) 
 
 ### 성능 요약
 
-메모리: 72.9 MB, 시간: 0.35 ms
+메모리: 75.7 MB, 시간: 0.03 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 11월 1일 16:29:19
+2023년 11월 4일 10:1:48
 
 ### 문제 설명
 
--- 프로그래머스/1/42862. 체육복/체육복.java
+++ 프로그래머스/1/42862. 체육복/체육복.java
@@ -2,44 +2,34 @@ import java.util.*;
 
 class Solution {
     public int solution(int n, int[] lost, int[] reserve) {
-        int answer = 0;
-        int[] count = new int[n+1];
+        int answer = n;
+        int[] count = new int[n];
         
-        //문제에서 lost나 reserve가 정렬되어 있다는 언급이 없기 때문에 예제만 보고 정렬되어 있다고 가정하면 안 됩니다.
-        Arrays.sort(lost);
-        Arrays.sort(reserve);
-        Arrays.fill(count,1);
         
         for(int num:reserve){
-            count[num]++;
+            count[num-1]++;
         }
         // 여벌 체육복이 있는 학생도 도난당할 수 있습니다. 즉, reserve에 속한 원소가 lost에도 속할 수 있습니다.
         for(int num:lost){
-            count[num]--;
+            count[num-1]--;
         }
       
-        for(int num:lost){
-            if(count[num]!=0)
-                continue;
-            
-            //체육복 없는 학생번호
-           if(num-1 >=1 && count[num-1] == 2){
-                count[num-1]--;
-                count[num]++;
-                continue;
-           }
-           
-           if(num+1 <= n && count[num+1] == 2){
-                count[num+1]--;
-                count[num]++;
-            }
-            
+        for(int i=0;i<n;i++){
+            if(count[i]== -1){
+                //체육복 없다.
+                if(i>0 && count[i-1] == 1){
+                    count[i-1]--;
+                    count[i]++;
+                }else if(i<n-1 && count[i+1]==1){
+                    count[i+1]--;
+                    count[i]++;
+                }else{
+                    answer--;
+                }
+            }   
         }
         
-        for(int i=1;i<=n;i++){
-            if(count[i]>0)
-                answer++;
-        }
+        
         
         return answer;
     }
