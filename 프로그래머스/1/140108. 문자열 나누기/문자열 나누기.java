d4c923b304ba86df162d88fd1491006166af0163
leeminju <leemj4090@naver.com>
2023-12-01 09:14:35 +0900

    [level 1] Title: 문자열 나누기, Time: 0.76 ms, Memory: 83.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int solution(String s) {
+        int answer = 1;
+        int start = 0;
+        char x = s.charAt(0);
+        int same=0;
+        int not=0;
+        System.out.println(x);
+        
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i) == x){
+                same++;
+            }else{
+                not++;
+            }
+            
+            if(same == not && same > 0){
+               // String split = s.substring(start, i+1);
+                if(i < s.length()-1){
+                    i++;
+                    start =i;
+                    same = 1;
+                    not = 0;
+                    x= s.charAt(i);
+                    answer++;
+                }
+            } 
+        }
+        
+        
+        
+        
+        
+        return answer;
+    }
+}
