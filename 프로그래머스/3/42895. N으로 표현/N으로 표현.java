cc56c0ab41b57f3efa53ddd0b5d29a5b15fb4cd5
leeminju <leemj4090@naver.com>
2023-12-04 22:23:50 +0900

    [level 3] Title: N으로 표현, Time: 0.01 ms, Memory: 74.6 MB -BaekjoonHub

---
+++ 프로그래머스/3/42895. N으로 표현/N으로 표현.java
@@ -0,0 +1,48 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int N, int number) {
+        boolean find = false;
+        
+        if(N == number)
+            return 1;
+            
+        HashSet<Integer>[] sets = new HashSet[9];
+        for(int i=1;i<9;i++){
+            sets[i] = new HashSet<>();
+            if(i == 1){
+                sets[1].add(N);// 1번 사용해서 만들 수 있는 수 -> N
+                continue;
+            }             
+            sets[i].add(N * makeRepeatOneNum(i));
+            
+            for(int j=1;j<= i-1;j++){
+                for(int num1 :sets[j]){
+                    for(int num2 : sets[i-j]){
+                       sets[i].add(num1 * num2);
+                    if(num1 >= num2)
+                        sets[i].add(num1 / num2);
+                    if(num1 > num2)
+                        sets[i].add(num1 - num2);
+                    sets[i].add(num1 + num2);
+                    }
+                }
+            }
+            
+            if(sets[i].contains(number)){
+                return i;
+            }
+        
+        }
+    
+        return -1;
+    }
+    
+    int makeRepeatOneNum(int count){
+        int num = 0;
+        for(int i=0;i<count;i++){
+            num += Math.pow(10,i);
+        }
+        return num;  
+    }
+}
