caa3e7094efb774ea1f91c3cbd391f81eadff8e7
leeminju <leemj4090@naver.com>
2023-12-05 21:15:56 +0900

    [level 1] Title: 햄버거 만들기, Time: 0.12 ms, Memory: 73.4 MB -BaekjoonHub

--- 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.java
+++ 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.java
@@ -8,26 +8,21 @@ class Solution {
         for(int ing :ingredient){
             stack.push(ing);
                
-            while(stack.size() >= 4){
-                if(stack.peek()== 1){
-                    int fourth = stack.pop();
-                    int third = stack.pop();
-                    int second =stack.pop();
-                    int first = stack.pop();
+            if(stack.size() >= 4 && stack.peek()== 1){
+                int fourth = stack.pop();
+                int third = stack.pop();
+                int second =stack.pop();
+                int first = stack.pop();
                 
-                    if(first == 1 && second== 2 && third== 3){
-                        answer++;
-                    }else{
-                        //원상 복구
-                        stack.push(first);
-                        stack.push(second);
-                        stack.push(third);
-                        stack.push(fourth);
-                        break;
-                    }
+                if(first == 1 && second== 2 && third== 3){
+                     answer++;
                 }else{
-                    break;
-                }       
+                    //원상 복구
+                    stack.push(first);
+                    stack.push(second);
+                    stack.push(third);
+                    stack.push(fourth);
+                }    
             }
             
         }
