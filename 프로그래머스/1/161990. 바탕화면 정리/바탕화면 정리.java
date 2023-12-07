1068193f86cd3fb3e2426c50a697745897312b71
leeminju <leemj4090@naver.com>
2023-12-07 09:42:25 +0900

    [level 1] Title: 바탕화면 정리, Time: 0.17 ms, Memory: 73.5 MB -BaekjoonHub

--- 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.java
+++ 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.java
@@ -1,10 +1,7 @@
 class Solution {
     public int[] solution(String[] wallpaper) {
         int[] answer = new int[4];
-        int minx = 51;
-        int maxx = -1;
-        int miny = 51;
-        int maxy = -1;
+        int minx = 51,maxx = -1,miny = 51, maxy = -1;
         
         for(int i=0;i<wallpaper.length;i++){
             for(int j=0;j<wallpaper[i].length();j++){
@@ -16,10 +13,8 @@ class Solution {
                 }
             }
         }
-        answer[0]=minx;
-        answer[1]=miny;
-        answer[2]=maxx+1;
-        answer[3]=maxy+1;
+        
+        answer = new int[]{minx,miny,maxx+1,maxy+1};
     
         
         return answer;
