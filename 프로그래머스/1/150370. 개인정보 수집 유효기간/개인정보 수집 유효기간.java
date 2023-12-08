33e316711ba3ff5f9d1c4ee2cae0a58f89a21a6f
leeminju <leemj4090@naver.com>
2023-12-08 11:17:44 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 0.67 ms, Memory: 73.9 MB -BaekjoonHub

--- 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
@@ -1,47 +1,60 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
 class Solution {
     public int[] solution(String today, String[] terms, String[] privacies) {
-        List<Integer> list=new ArrayList<>();
-            
-        int year = Integer.parseInt(today.substring(0,4));
-        int month = Integer.parseInt(today.substring(5,7));
-        int day = Integer.parseInt(today.substring(8));
-                                   
-        int todayNum = (year - 1) * (28 * 12)  + (month -1) * 28 + day;
         
-        Map<String,Integer> termsMap = new HashMap<>();
+        ArrayList<Integer> answer=new ArrayList<Integer>();
+        HashMap<String,Integer> AgreementMap=new HashMap<>();
+
+        int idx=0;
         
-        for(String str:terms){
-            String kind =str.split(" ")[0];
-            int term= Integer.parseInt(str.split(" ")[1]);
-            termsMap.put(kind,term);
-        }
-        int num=0;
+        String[] t=today.split("\\.");
+		int todayCnt = Integer.parseInt(t[0]) * 10000 + Integer.parseInt(t[1]) * 100 + Integer.parseInt(t[2]);
         
-        for(String privacy : privacies){
-             num++;
-             String startDate = privacy.split(" ")[0];
-             int syear = Integer.parseInt(startDate.substring(0,4));
-             int smonth = Integer.parseInt(startDate.substring(5,7));
-             int sday = Integer.parseInt(startDate.substring(8));
-                                   
-            int sDateNum = (syear - 1) * (28 * 12)  + (smonth -1) * 28 + sday;
-            
-            
-            String kind = privacy.split(" ")[1];
-            int term = termsMap.get(kind);
-            sDateNum += term * 28;// 만료일
-            if(todayNum >= sDateNum){
-                list.add(num);
-            }
+    
+        for(String str:terms)
+        {
+        	String[] st=str.split(" ");
+        	AgreementMap.put(st[0], Integer.parseInt(st[1]));//계약 당 기간 저장
         }
         
-        int[] answer  = new int[list.size()];
-        for(int i=0;i<list.size();i++){
-            answer[i]=list.get(i);
+        for(String str:privacies)
+        {
+        	idx++;//1부터 시작
+        	
+        	int year=Integer.parseInt(str.substring(0, 4));
+        	int month=Integer.parseInt(str.substring(5, 7));
+        	int day=Integer.parseInt(str.substring(8, 10));
+        	int term=AgreementMap.get(str.substring(11, 12));//유효기간
+        	int end=0;
+        	month += term;
+        	
+        	if(month>12) {
+        		year+=month/12;
+        		if(month%12==0){
+                    month=12;
+                    year--;
+                }
+                else{
+                    month%=12;
+                }
+        	}
+        	end=year*10000+month*100+day;
+        	
+        	if(todayCnt >= end)//지났으면 파기
+            {
+                answer.add(idx);
+        	}
         }
         
+        int[] answer2=new int[answer.size()];
+        for(int i=0;i<answer.size();i++){
+            answer2[i]=answer.get(i);
+        }
+      
         
-        return answer;
+        return answer2;
     }
+    
+    
 }
