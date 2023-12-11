8449d2097330d627fc3aea5573b8db17a6faa96d
leeminju <leemj4090@naver.com>
2023-12-11 12:19:21 +0900

    [level 1] Title: 공원 산책, Time: 0.18 ms, Memory: 71.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/172928. 공원 산책/공원 산책.java
@@ -0,0 +1,87 @@
+class Solution {
+    public int[] solution(String[] park, String[] routes) {
+        int[] answer = {};
+        
+        int w = park.length;
+        int h = park[0].length();
+        int[] start = new int[2];
+        
+        for(int i=0;i<park.length;i++){
+            if(park[i].contains("S")){
+                int j = park[i].indexOf("S");
+                start = new int[]{i,j};
+            }
+        }
+        
+        for(String route:routes){
+            String direction = route.split(" ")[0];
+            int distance= Integer.parseInt(route.split(" ")[1]);
+            int count = 1;
+            boolean isOk = true;
+            
+            if(direction.equals("N")){
+                if(start[0] - distance >= 0){
+                    while(count <= distance){
+                        if(park[start[0]-(count)].charAt(start[1]) == 'X'){
+                            isOk=false;
+                            break;
+                        }
+                        count++;
+                    }
+                    
+                    if(isOk){
+                        start[0] -= distance;
+                    }
+                }    
+            }else if(direction.equals("S")){
+                 if(start[0] + distance < w){
+                    while(count <= distance){
+                        if(park[start[0]+count].charAt(start[1]) == 'X'){
+                            isOk=false;
+                            break;
+                        }
+                        count++;
+                    }
+                    
+                    if(isOk){
+                        start[0] += distance;
+                    }
+                } 
+            }else if(direction.equals("W")){
+                 if(start[1] - distance >= 0){
+                    while(count <= distance){
+                        if(park[start[0]].charAt(start[1]-count) == 'X'){
+                            isOk=false;
+                            break;
+                        }
+                        count++;
+                    }
+                    
+                    if(isOk){
+                        start[1] -= distance;
+                    }
+                }    
+            }else if(direction.equals("E")){
+                if(start[1] + distance < h){
+                    while(count <= distance){
+                        if(park[start[0]].charAt(start[1]+count) == 'X'){
+                            isOk = false;
+                            break;
+                        }
+                        count++;
+                    }
+                    
+                    if(isOk){
+                        start[1] += distance;
+                    }
+                }
+            }
+            
+            
+        }
+        
+        
+        
+        return start;
+    }
+}
