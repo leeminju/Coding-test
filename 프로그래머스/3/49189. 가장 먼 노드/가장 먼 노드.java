dddd013ed47e352d76a5886434e14204662ab747
leeminju <leemj4090@naver.com>
2023-12-20 10:04:46 +0900

    [level 3] Title: 가장 먼 노드, Time: 39.92 ms, Memory: 103 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
@@ -42,8 +42,7 @@ class Solution {
         d[1] = 0;
     
         
-        while(!q.isEmpty()) { // 큐가 비어있지 않다면
-            // 가장 최단 거리가 짧은 노드에 대한 정보 꺼내기
+        while(!q.isEmpty()) { 
             Node node = q.poll(); // 현재 노드
             int now = node.getIndex();
             if(visited[now]){
@@ -51,14 +50,13 @@ class Solution {
             }
             visited[now] = true;
             
-            // 현재 노드와 연결된 다른 인접한 노드들을 확인
             for (int i = 0; i < graph.get(now).size(); i++) {
-                int cost = d[now] + 1;
+                int distance = d[now] + 1;
                 int next = graph.get(now).get(i);
-                // 현재 노드를 거쳐서, 다른 노드로 이동하는 거리가 더 짧은 경우
-                if (cost < d[next] && !visited[next]) {
-                    d[next] = cost;
-                    q.offer(new Node(next,cost));
+                
+                if (distance < d[next] && !visited[next]) {
+                    d[next] = distance;
+                    q.offer(new Node(next,distance));
                 }
             }
         }
@@ -74,8 +72,6 @@ class Solution {
                 count++;
             }
         }
-        
-        
         
         return count;
     }
