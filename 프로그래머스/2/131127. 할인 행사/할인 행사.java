a2e27ddc2e8935904e68c32e5dd43142a703343a
leeminju <leemj4090@naver.com>
2023-12-26 08:28:31 +0900

    [level 2] Title: 할인 행사, Time: 0.04 ms, Memory: 74.8 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.java
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.java
@@ -3,13 +3,7 @@ class Solution {
     public int solution(String[] want, int[] number, String[] discount) {
         int answer = 0;
         Map<String,Integer> map = new HashMap<>();
-        Map<String,Integer> answer_map = new HashMap<>();
-        
-        for(int i=0;i<want.length;i++){
-            answer_map.put(want[i],number[i]);
-        }
-        
-        
+         
         for(int i=0;i<discount.length;i++){
             if(i >= 10){
                 String remove = discount[i-10]; 
@@ -22,7 +16,7 @@ class Solution {
             map.put(key,map.getOrDefault(key,0)+1);
             
             if(i >= 9){
-                if(isOk(answer_map,map)){
+                if(isOk(want,number,map)){
                     answer++;
                 } 
             }
@@ -32,11 +26,14 @@ class Solution {
         return answer;
     }
     
-    boolean isOk(Map<String,Integer> answer_map,Map<String,Integer> map){
-        for(String key:answer_map.keySet()){
+    boolean isOk(String[] want, int[] number, Map<String,Integer> map){
+        for(int i=0;i<want.length;i++){
+            String key = want[i];
+            int value = number[i];
+            
             if(!map.containsKey(key)){
                 return false;
-            }else if(map.get(key) < answer_map.get(key)){
+            }else if(map.get(key) < value){
                 return false;
             }
         }
