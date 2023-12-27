2194c6f50d268f9fba0d20cb7c197ea12f9f60b0
leeminju <leemj4090@naver.com>
2023-12-27 09:29:09 +0900

    [level 2] Title: 프로세스, Time: 1.08 ms, Memory: 79.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -0,0 +1,46 @@
+import java.util.*;
+
+public class Node{
+    int index;
+    int value;
+    
+    public Node(int index,int value){
+        this.index = index;
+        this.value = value;
+    }
+    
+}
+
+class Solution {
+    public int solution(int[] priorities, int location) {
+        int answer = 0;
+        
+        Queue<Node> q = new LinkedList<>();
+        PriorityQueue<Node> pq = new PriorityQueue<>((o1,o2)->o2.value - o1.value);
+        
+        for(int i=0;i<priorities.length;i++){
+            Node node = new Node(i,priorities[i]);
+            pq.offer(node);
+            q.offer(node);
+        }
+        
+        
+        while(!q.isEmpty()){
+            Node node = q.poll();
+            
+            if(node.value < pq.peek().value){
+                q.offer(node);// 뒤에 넣기
+            }else{
+                pq.poll();
+                answer++;
+                
+                if(node.index == location){
+                    return answer;
+                }
+            }
+            
+        }
+        
+        return answer;
+    }
+}
