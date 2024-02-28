38bb020bf4b420113d4090120690e706c24c304a
leeminju <leemj4090@naver.com>
2024-02-28 15:59:59 +0900

    [level 2] Title: 2개 이하로 다른 비트, Time: 110.66 ms, Memory: 119 MB -BaekjoonHub

--- 프로그래머스/2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.java
+++ 프로그래머스/2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.java
@@ -8,7 +8,6 @@ class Solution {
                 answer[i] = numbers[i]+1;
             }else{
                 String num = Long.toString(numbers[i],2);    
-                //System.out.println(num);
             
                 StringBuilder sb = new StringBuilder(num);
                 int index = num.lastIndexOf("0");// 뒤에서 처음으로 0이 나오는 index
@@ -19,14 +18,12 @@ class Solution {
                     index = 0;
                 }
                 
-                System.out.println(index);
                 
                 //'01' - > '10'
                 sb.setCharAt(index,'1');
                 sb.setCharAt(index+1,'0');
                 
-               // System.out.println(sb.toString());
-                //String(2진수)를 10진수로 변환
+        
                 answer[i]=Long.parseLong(sb.toString(), 2);
             }
             
