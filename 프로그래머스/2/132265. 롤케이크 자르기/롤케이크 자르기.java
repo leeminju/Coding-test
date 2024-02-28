bddd8404db3b75d0623e6a67152621cd64c7c744
leeminju <leemj4090@naver.com>
2024-02-28 13:15:02 +0900

    [level 2] Title: 롤케이크 자르기, Time: 178.48 ms, Memory: 190 MB -BaekjoonHub

--- 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.java
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.java
@@ -21,11 +21,19 @@ class Solution {
        }
        map2.put(topping[i],map2.getOrDefault(topping[i],0)+1);
        
-    //동생이 뺏을 때마다, 각자 가지고 있는 토핑의 종류가 같은지 확인한다.
-    //같다면 답을 추가한다.
+        //동생이 더 많아지면 종료 시킴
+        if(map.size() < map2.size()){
+            break;
+        }
+       
+       //동생이 뺏을 때마다, 각자 가지고 있는 토핑의 종류가 같은지 확인한다.
+       //같다면 답을 추가한다.
        if(map.size() == map2.size()){
            answer++;
        }
+       
+      
+       
     }
         return answer;
     }
