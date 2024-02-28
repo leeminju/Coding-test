7c15c1cf62b864d56b2a19574ceb8136f038a698
leeminju <leemj4090@naver.com>
2024-02-28 18:07:57 +0900

    [level 2] Title: 2개 이하로 다른 비트, Time: 84.16 ms, Memory: 117 MB -BaekjoonHub

--- 프로그래머스/2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.java
+++ 프로그래머스/2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.java
@@ -7,28 +7,31 @@ class Solution {
             if(numbers[i] % 4 != 3){
                 answer[i] = numbers[i]+1;
             }else{
-                String num = Long.toString(numbers[i],2);    
-            
-                StringBuilder sb = new StringBuilder(num);
-                int index = num.lastIndexOf("0");// 뒤에서 처음으로 0이 나오는 index
-     
-                //0이 없을 경우
-                if(index == -1){
-                    sb.insert(0,"0");//맨앞에 0 추가
-                    index = 0;
-                }
-                
-                
-                //'01' - > '10'
-                sb.setCharAt(index,'1');
-                sb.setCharAt(index+1,'0');
-                
-        
-                answer[i]=Long.parseLong(sb.toString(), 2);
+                answer[i]=function(numbers[i]);
             }
             
         }
         
         return answer;
+    }
+    
+    
+    long function(long number){
+        String num = Long.toString(number,2);    
+            
+        StringBuilder sb = new StringBuilder(num);
+        int index = num.lastIndexOf("0");// 뒤에서 처음으로 0이 나오는 index
+     
+        //0이 없을 경우
+        if(index == -1){
+            sb.insert(0,"1");//맨앞에 1 추가
+            sb.setCharAt(1,'0');
+        }else{
+            //'01' - > '10'
+            sb.setCharAt(index,'1');
+            sb.setCharAt(index+1,'0');
+        }
+                    
+        return Long.parseLong(sb.toString(), 2);
     }
 }
