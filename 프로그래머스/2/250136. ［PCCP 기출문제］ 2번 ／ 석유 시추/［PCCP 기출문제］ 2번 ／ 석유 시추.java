094e0233fe3080be1f61f79e3c1e022ca16876c9
leeminju <leemj4090@naver.com>
2024-03-04 21:12:12 +0900

    [level 2] Title: [PCCP 기출문제] 2번 / 석유 시추, Time: 21.09 ms, Memory: 63.1 MB -BaekjoonHub

--- 프로그래머스/2/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.java
+++ 프로그래머스/2/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.java
@@ -1,11 +1,10 @@
 import java.util.*;
 class Solution {
-
     int n,m;
-    int starty = 500;
-    int endy = -1;
-    int count = 0;
+    int starty,endy;
+    int[][] land;
     public int solution(int[][] land) {
+        this.land = land;
         int answer = 0;
         n = land.length;//세로
         m = land[0].length;//가로
@@ -15,14 +14,13 @@ class Solution {
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(land[i][j] == 1){
-                    count = 0;
-                    starty= m-1;
+                    starty= m;
                     endy = 0;
-                    find(land,i,j,index);//연결된 덩어리에 같은 숫자(1이 아닌 숫자)로 값을 바꿔주고 한 덩어리의 크기 계산
-                    
+                    int count = find(i,j,index);
                     // 덩어리의 가로 시작부터 끝까지 
+                    
                     for(int k=starty;k<=endy;k++){
-                        arr[k] += count;
+                        arr[k] += count;//연결된 덩어리에 같은 숫자(1이 아닌 숫자)로 값을 바꿔주고 한 덩어리의 크기 계산
                     }
                     index++;// 다음 덩어리는 증가된 숫자 저장되게함(중복 방지)
                 }
@@ -37,9 +35,14 @@ class Solution {
         return answer;
     }
     
-    void find(int[][] land, int x,int y,int value){        
+    int find(int x,int y,int value){        
         int[] dx = {-1,1,0,0};
         int[] dy = {0,0,-1,1};
+        int count = 1;
+        land[x][y] = value;
+        starty = Math.min(starty,y);
+        endy= Math.max(endy,y);
+    
         Queue<int[]> q = new LinkedList<>();
         q.offer(new int[]{x,y});
         
@@ -47,20 +50,14 @@ class Solution {
             int[] node= q.poll();
             int cx = node[0];
             int cy = node[1];
-            
-            if(land[cx][cy] != 1)
-                continue;
-                    
-            starty= Math.min(starty,cy);// 덩어리의 가로 시작 위치 저장
+            starty = Math.min(starty,cy);
             endy= Math.max(endy,cy);// 덩어리의 가로 끝 위치 저장
-            land[cx][cy] = value;// 같은 덩어리는 모두 같은 값으로 바꾼다(1이 아닌 숫자)
-            count++;// 덩어리의 크기 증가
-        
+            
             //현재 위치에서 상하좌우로 이동한다.
             for(int i=0;i<4;i++){
                 int nx = cx + dx[i];
                 int ny = cy + dy[i];
-            
+                
                 if(nx < 0 || ny < 0 || nx >= n || ny >= m){
                     continue;
                 }
@@ -69,9 +66,11 @@ class Solution {
                     continue;
                 
                 q.offer(new int[]{nx,ny});
+                land[nx][ny] = value;
+                count++;// 덩어리의 크기 증가
             }
         }
         
-        
+        return count;
     }
 }
