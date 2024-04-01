44fff9cdcfd9bca7dc4f82ff273d9d2d316b54a5
leeminju <leemj4090@naver.com>
2024-04-01 16:45:57 +0900

    [level 2] Title: 택배상자, Time: 179.52 ms, Memory: 178 MB -BaekjoonHub

---
+++ 프로그래머스/2/131704. 택배상자/택배상자.java
@@ -0,0 +1,45 @@
+import java.util.*;
+class Solution {
+    public int solution(int[] order) {
+        int answer = 0;// 몇 개의 상자를 실을 수 있는지 
+        //현재 컨테이너
+        Queue<Integer> main = new LinkedList<>();
+        //보조 컨테이너
+        Stack<Integer> sub = new Stack<>();
+            
+        int count = 1;
+        for(int i=0;i<order.length;i++){
+            main.offer(count++);
+        }
+
+        int index = 0;
+
+        while(!main.isEmpty()){
+            if(main.peek() != order[index]){
+                if(!sub.isEmpty() && sub.peek() == order[index]){
+                    sub.pop();
+                    index++;
+                    answer++;//트럭에 싣다.
+                }else{
+                    sub.push(main.poll());//보조컨테이너에 잠시 보관
+                }
+            }else if(main.peek() == order[index]){
+                main.poll();
+                index++;
+                answer++;//트럭에 싣다.
+            }
+        }
+        
+        while(!sub.isEmpty()){
+             if(sub.peek() == order[index]){
+                sub.pop();
+                index++;
+                answer++;//트럭에 싣다.
+            }else{
+                break;
+            }
+        }
+            
+        return answer;
+    }
+}
