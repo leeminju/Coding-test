4541e1ae43585854747a0e14abaa47854749b606
leeminju <leemj4090@naver.com>
2024-07-30 15:08:24 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 20.74 ms, Memory: 73.7 MB -BaekjoonHub

--- 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.java
+++ 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.java
@@ -1,12 +1,19 @@
 class Solution {
     public String solution(String my_string, int num1, int num2) {
-        StringBuilder sb = new StringBuilder(my_string);
-        char c1=sb.charAt(num1);
-        char c2=sb.charAt(num2);
+        String answer = "";
+        char c1=my_string.charAt(num1);
+        char c2=my_string.charAt(num2);
+
+        for(int i=0;i<my_string.length();i++){
+            if(i == num1){
+                answer += c2;
+            }else if( i == num2){
+                answer += c1;
+            }else{
+                answer +=  my_string.charAt(i);
+            }
+        }
         
-        sb.replace(num1,num1+1,Character.toString(c2));
-        sb.replace(num2,num2+1,Character.toString(c1));
-        
-        return sb.toString();
+        return answer;
     }
 }
