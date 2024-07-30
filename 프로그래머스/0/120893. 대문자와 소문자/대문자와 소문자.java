2ed63641cb2307cc7499104b0874935d6c7e46a7
leeminju <leemj4090@naver.com>
2024-07-30 14:50:20 +0900

    [level 0] Title: 대문자와 소문자, Time: 16.32 ms, Memory: 79.3 MB -BaekjoonHub

--- 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.java
+++ 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.java
@@ -1,17 +1,16 @@
 class Solution {
     public String solution(String my_string) {
         String answer = "";
-        int dif = (int)'A' - 'a';
         
         for(int i=0;i<my_string.length();i++){
             char c = my_string.charAt(i);
             
             //소문자
             if(c >= 'a' && c<= 'z'){
-                answer += (char)(c  + dif);
+                answer += (char)(c  - 32);
             }else{
                 //대문자
-                answer += (char)(c  - dif);
+                answer += (char)(c  + 32);
             }
             
         }
