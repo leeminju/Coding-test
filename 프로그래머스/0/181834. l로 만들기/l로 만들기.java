af70c8821c5a6e756618562f4808bbff7aa376f7
leeminju <leemj4090@naver.com>
2024-08-07 18:26:34 +0900

    [level 0] Title: l로 만들기, Time: 1144.78 ms, Memory: 386 MB -BaekjoonHub

---
+++ 프로그래머스/0/181834. l로 만들기/l로 만들기.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String solution(String myString) {
+        String answer = "";
+        
+        for(int i=0;i<myString.length();i++){
+            char c = myString.charAt(i);
+            if(c <'l'){
+                answer += "l";
+            }else{
+                answer += c;
+            }
+        }
+        
+        return answer;
+    }
+}
