c61339019716274ec40420ef2cffc67dd0844217
leeminju <leemj4090@naver.com>
2024-08-14 21:12:03 +0900

    [level 0] Title: 코드 처리하기, Time: 390.60 ms, Memory: 384 MB -BaekjoonHub

---
+++ 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.java
@@ -0,0 +1,33 @@
+class Solution {
+    public String solution(String code) {
+        String ret = "";
+        int mode = 0;
+        
+        for(int i=0;i<code.length();i++){
+            char c = code.charAt(i);
+            if(mode == 0){
+                if(c !='1'){
+                    if(i%2==0){
+                        ret += c;
+                    }
+                }else{
+                    mode=1;
+                }
+            }else if(mode == 1){
+                if(c !='1'){
+                    if(i%2==1){
+                        ret += c;
+                    }
+                }else{
+                    mode=0;
+                }
+            }
+        }
+        
+        if(ret.length()==0){
+            return "EMPTY";
+        }
+        
+        return ret;
+    }
+}
