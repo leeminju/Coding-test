24f1af960c2b4cc5ff2bb48965f42b563c124253
leeminju <leemj4090@naver.com>
2024-08-16 17:23:44 +0900

    [level 0] Title: 다음에 올 숫자, Time: 0.06 ms, Memory: 73.8 MB -BaekjoonHub

--- 프로그래머스/0/120924. 다음에 올 숫자/다음에 올 숫자.java
+++ 프로그래머스/0/120924. 다음에 올 숫자/다음에 올 숫자.java
@@ -2,22 +2,12 @@ class Solution {
     public int solution(int[] common) {
         int answer = 0;
         int d = common[1]-common[0];
-   
         
-        if(is_등차수열(common,d)){
+        if(d == common[2]-common[1]){
             return common[common.length-1]+ d;
         }else{
             int r = common[1]/common[0];
             return common[0] * (int)Math.pow(r,common.length);
         }
-    }
-    
-    boolean is_등차수열(int[] array,int d){
-        for(int i=1;i<array.length;i++){
-            if(d  != array[i]-array[i-1]){
-                return false;
-            }
-        }
-        return true;
     }
 }
