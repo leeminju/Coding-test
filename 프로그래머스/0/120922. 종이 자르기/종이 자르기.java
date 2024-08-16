fca24dbc5a79b1f023ea4a783607af5554e54801
leeminju <leemj4090@naver.com>
2024-08-16 18:04:52 +0900

    [level 0] Title: 종이 자르기, Time: 0.12 ms, Memory: 76.8 MB -BaekjoonHub

--- 프로그래머스/0/120922. 종이 자르기/종이 자르기.java
+++ 프로그래머스/0/120922. 종이 자르기/종이 자르기.java
@@ -3,10 +3,6 @@ class Solution {
         int min = Math.min(M,N);
         int max = Math.max(M,N);
         
-        if(M == 1 && N == 1){
-            return 0;
-        }
-        
         return (min - 1) + (min * (max-1));
     }
 }
