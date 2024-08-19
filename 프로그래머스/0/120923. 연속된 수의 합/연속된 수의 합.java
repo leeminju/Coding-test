a5c98330e2432ead5e9554e05cb4e39adcf0aac0
leeminju <leemj4090@naver.com>
2024-08-19 17:49:36 +0900

    [level 0] Title: 연속된 수의 합, Time: 0.03 ms, Memory: 75 MB -BaekjoonHub

--- 프로그래머스/0/120923. 연속된 수의 합/연속된 수의 합.java
+++ 프로그래머스/0/120923. 연속된 수의 합/연속된 수의 합.java
@@ -1,22 +1,20 @@
 class Solution {
     public int[] solution(int num, int total) {
         int[] answer = new int[num];
-        int count = num;
-        int center = total/num;
-        int idx=num/2;
-        if(total % num == 0){
-            answer[idx] = center;
-        }else{
-            idx--;
-            answer[idx]=center;
-        }
+    
+        int center = total/num;//중앙값
+        int idx= (total%num == 0)? num/2 : num/2-1;//중앙값의 인덱스
+        
+        answer[idx]=center;//중앙값 배열에 저장
         
         int n = center;
+        //중앙에서부터 0까지 숫자 감소
         for(int i = idx-1;i>=0;i--){
             answer[i] = --n;
         }
         
         n = center;
+        //중앙에서부터 마지막까지 숫자 증가 
         for(int i = idx+1;i<num;i++){
             answer[i] = ++n;
         }
